661949ef5afd6f8585221cb5e5508a5490f77643
Aeroluna <aeroluna5@gmail.com>
2021-03-22 13:20:59 -0700

    null check for gradientbackground

--- Technicolor/HarmonyPatches/BloomPrePassBackgroundColorsGradientFromColorSchemeColors.cs
+++ Technicolor/HarmonyPatches/BloomPrePassBackgroundColorsGradientFromColorSchemeColors.cs
@@ -13,10 +13,13 @@
 
         internal static void SetGradientColors(Color colorLeft, Color colorRight)
         {
-            _bloomPrePassBackgroundColorsGradient.elements[0].color = colorLeft * _groundColorIntensity;
-            _bloomPrePassBackgroundColorsGradient.elements[1].color = colorLeft * _groundColorIntensity;
-            _bloomPrePassBackgroundColorsGradient.elements[5].color = colorRight * _skyColorIntensity;
-            _bloomPrePassBackgroundColorsGradient.UpdateGradientTexture();
+            if (_bloomPrePassBackgroundColorsGradient != null) 
+            {
+                _bloomPrePassBackgroundColorsGradient.elements[0].color = colorLeft * _groundColorIntensity;
+                _bloomPrePassBackgroundColorsGradient.elements[1].color = colorLeft * _groundColorIntensity;
+                _bloomPrePassBackgroundColorsGradient.elements[5].color = colorRight * _skyColorIntensity;
+                _bloomPrePassBackgroundColorsGradient.UpdateGradientTexture();
+            }
         }
 
         private static void Postfix(BloomPrePassBackgroundColorsGradient ____bloomPrePassBackgroundColorsGradient, float ____skyColorIntensity, float ____groundColorIntensity)
