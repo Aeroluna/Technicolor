882c3d1d9894de7205237ecc88680d39abe09e90
Jonathon Fitch <lightman1@virginmedia.com>
2022-04-04 18:44:01 +0100

    Allow Technicolor to be used on maps that use Chroma, but don't define a color for items

--- Technicolor/HarmonyPatches/TechniLights.cs
+++ Technicolor/HarmonyPatches/TechniLights.cs
@@ -33,7 +33,7 @@ namespace Technicolor.HarmonyPatches
                 foreach (ILightWithId light in lightColorizer.Lights)
                 {
                     Color color = TechnicolorController.GetTechnicolor(warm, beatmapEventData.time + light.GetHashCode(), TechnicolorConfig.Instance.TechnicolorLightsStyle);
-                    lightColorizer.Colorize(false, color, color, color, color);
+                    lightColorizer.Colorize(true, color, color, color, color);
                     __instance.Refresh(true, new[] { light }, beatmapEventData);
                 }
 
@@ -51,11 +51,11 @@ namespace Technicolor.HarmonyPatches
                 switch (TechnicolorConfig.Instance.TechnicolorLightsGrouping)
                 {
                     case TechnicolorLightsGrouping.ISOLATED_GROUP:
-                        lightColorizer.Colorize(false, color, color, color, color);
+                        lightColorizer.Colorize(true, color, color, color, color);
                         break;
 
                     default:
-                        _manager.GlobalColorize(false, color, color, color, color);
+                        _manager.GlobalColorize(true, color, color, color, color);
                         break;
                 }
             }
